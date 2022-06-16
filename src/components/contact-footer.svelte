841962b58fe077fc507113625ed5a904ff001a2e
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-06-16 10:35:32 -0700

    Site broke, fixing it

--- src/components/contact-footer.svelte
+++ src/components/contact-footer.svelte
@@ -4,13 +4,6 @@
 	import githubIcon from "/src/lib/Images/contact/github-icon.svg"
 	import linkedinIcon from "/src/lib/Images/contact/linkedin-icon.svg"
 	import twitterIcon from "/src/lib/Images/contact/twitter-icon.svg"
-	import { browser, dev } from '$app/env';
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
 </script>
 <div class="page-wrapper">
 	<header>
